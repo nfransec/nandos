ae1a837e02c7c21e7548e669d4e16e90f7e210d4
Nirmal Francis <nirmal.francis@nirmalf-wsmxmk3.internal.salesforce.com>
2024-09-07 21:08:18 +0530

    Implement the layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,35 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Nirmal', lastName: 'Francis', email: 'nirmal.francis@outlook.com'};
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1445846.86}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 16.71}, {currentBalance: 500.00}]} 
+      />
+    </section>
   )
 }
 
